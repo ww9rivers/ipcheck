97f44b88f8fcc3940867006f8fa33a91613fa234
Wei Wang <ww@9rivers.com>
2018-07-05 09:47:16 -0400

    Removed redering engine. Use simple inline HTML.

--- index.js
+++ index.js
@@ -5,14 +5,17 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
-    let xip = req.ip.replace(/^::ffff:/, '')
-    res.render('index', {
-	IP: xip,
-	browser: req.headers['user-agent']
-    })
+app.get('/*', (req, res) => {
+    res.send([
+	"<html><head><title>HITS IP Checker</title></head>",
+	'<body style="text-align:center; font-family:Verdana; background-color:#00274c; color:#ffcb05;">',
+	'<h1>Your IP is: </h1><p style="font-size:larger;font-weight:400;">',
+	req.ip.replace(/^::ffff:/, ''),
+	'</p><div style="font-size:x-small;font-weight:100;">Browser:<br/>',
+	req.headers['user-agent'],
+	"</div></body></html>"
+    ].join(''))
 })
 
 app.listen(5001, () => console.log('Example app listening on port 5001!'))
